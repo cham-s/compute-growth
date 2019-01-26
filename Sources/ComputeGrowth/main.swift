3b67801b6646f663a1e0faee6e0abfd06038450a
cham-s <cc.attou@gmail.com>
2019-01-26 18:45:27 +0100

    refactor a bit by encapsulating and renaming things

--- Sources/ComputeGrowth/main.swift
+++ Sources/ComputeGrowth/main.swift
@@ -1,69 +1,73 @@
 import Foundation
 
-enum Type {
-    case exponential, linear
-}
-
-struct FunctionData {
+// TODO: refactor furthermore since both linear and
+// exponential are tight toghether they should be part of the same type
+// This will avoid weird print call on the type itself.
+// and have an external shared starting point in time.
+// This would also simply initialisation of growth.
+struct Growth {
+    
+    enum GrowthType {
+        case exponential, linear
+    }
     
     var initialValue = 0.0
     var growth = 0.0
-    var type: Type
+    var type: GrowthType
     
-    init(initialValue: Double, growth: Double, type: Type) {
+    init(initialValue: Double, growth: Double, type: GrowthType) {
         self.initialValue = initialValue
         self.growth = growth
         self.type = type
     }
-}
-
-func yearlyValue(from data: FunctionData,and year: Int) -> Double {
-    switch data.type {
-    case .linear:
-        return data.initialValue + data.growth * Double(year)
-    case .exponential:
-        return data.initialValue * pow(data.growth, Double(year))
-    }
-}
-
-
-// TODO: refactor obviously
-func printHeader() {
-    print(String(format: "%@\n| % 12@ | % 12@ | % 12@ |\n%@",
-                 String(repeating: "-", count: 12 * 3 + 4 + 6),
-                 "x".padding(toLength: 12, withPad: " ", startingAt: 0),
-                 "linear".padding(toLength: 12, withPad: " ", startingAt: 0),
-                 "exponential".padding(toLength: 12, withPad: " ", startingAt: 0),
-                 String(repeating: "-", count: 12 * 3 + 4 + 6)))
-}
-
-func printResult(linear: FunctionData,
-                 exponential: FunctionData,
-                 startingYear: Int) {
     
-    var year = startingYear
-    var index = 0
-    
-    printHeader()
-    
-    while true {
-        let linearComponent = yearlyValue(from: linear, and: index)
-        let exponentialComponent = yearlyValue(from: exponential, and: index)
-        
-        print(String(format: "| %-12d | %-12.2f | %-12.2f |", year,
-                     linearComponent,
-                     exponentialComponent))
-        if linearComponent < exponentialComponent {
-            break
+    public func value(atTime: Int) -> Double {
+        switch type {
+        case .linear:
+            return initialValue + growth * Double(atTime)
+        case .exponential:
+            return initialValue * pow(growth, Double(atTime))
         }
+    }
+    
+    static func printResult(linear: Growth, exponential: Growth, start: Int) {
         
-        index +=  1
-        year +=  1
+        var year = start
+        var index = 0
+        
+        printHeader()
+        
+        while true {
+            let linearComponent = linear.value(atTime: index)
+            let exponentialComponent = exponential.value(atTime: index)
+            
+            print(String(format: "| %-12d | %-12.2f | %-12.2f |", year,
+                         linearComponent,
+                         exponentialComponent))
+            if linearComponent < exponentialComponent {
+                break
+            }
+            
+            index +=  1
+            year +=  1
+        }
+    }
+    
+    // TODO: extract printing from this type?
+    // Definitely refactor this this is ugly
+    static func printHeader() {
+        print(String(format: "%@\n| % 12@ | % 12@ | % 12@ |\n%@",
+                     String(repeating: "-", count: 12 * 3 + 4 + 6),
+                     "x".padding(toLength: 12, withPad: " ", startingAt: 0),
+                     "linear".padding(toLength: 12, withPad: " ", startingAt: 0),
+                     "exponential".padding(toLength: 12, withPad: " ", startingAt: 0),
+                     String(repeating: "-", count: 12 * 3 + 4 + 6)))
     }
 }
 
-let linear = FunctionData(initialValue: 5, growth: 11, type: .linear)
-let exponential = FunctionData(initialValue: 2, growth: 3, type: .exponential)
+
+let linear = Growth(initialValue: 25_000, growth: 500, type: .linear)
+let exponential = Growth(initialValue: 20_000, growth: 1.05, type: .exponential)
 let startingYear = 1
 
-printResult(linear: linear, exponential: exponential, startingYear: startingYear)
+Growth.printResult(linear: linear, exponential: exponential, start: startingYear)
