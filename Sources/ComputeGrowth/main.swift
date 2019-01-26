92d83abf3b098651b29d4eed7ac830b0aa9c4521
cham-s <cc.attou@gmail.com>
2019-01-26 19:49:36 +0100

    refactored code by separating logic

--- Sources/ComputeGrowth/main.swift
+++ Sources/ComputeGrowth/main.swift
@@ -1,16 +1,14 @@
 import Foundation
 
-// TODO: refactor furthermore since both linear and
-// exponential are tight toghether they should be part of the same type
-// This will avoid weird print call on the type itself.
-// and have an external shared starting point in time.
-// This would also simply initialisation of growth.
-struct Growth {
-    
-    enum GrowthType {
-        case exponential, linear
-    }
-    
+
+// Temporary name
+
+
+enum GrowthType {
+    case linear, exponential
+}
+
+struct GrowthData {
     var initialValue = 0.0
     var growth = 0.0
     var type: GrowthType
@@ -29,45 +27,53 @@ struct Growth {
             return initialValue * pow(growth, Double(atTime))
         }
     }
-    
-    static func printResult(linear: Growth, exponential: Growth, start: Int) {
+}
+
+struct Growth {
+    var linear: GrowthData
+    var exponential: GrowthData
+    var start: Int
+    var description: String {
+        var pointInTime = start
+        var output = header()
         
-        var year = start
-        var index = 0
-        
-        printHeader()
-        
-        while true {
-            let linearComponent = linear.value(atTime: index)
-            let exponentialComponent = exponential.value(atTime: index)
+        for i in (0...Int.max) {
+            let linearValue = linear.value(atTime: i)
+            let exponentialValue = exponential.value(atTime: i)
             
-            print(String(format: "| %-12d | %-12.2f | %-12.2f |", year,
-                         linearComponent,
-                         exponentialComponent))
-            if linearComponent < exponentialComponent {
-                break
-            }
-            
-            index +=  1
-            year +=  1
+            output += (String(format: "| %-12d | %-12.2f | %-12.2f |\n",
+                         pointInTime,
+                         linearValue,
+                         exponentialValue))
+            if linearValue < exponentialValue { break }
+            pointInTime +=  1
         }
+        return output
+    }
+    
+    init(linear: GrowthData, exponential: GrowthData, start: Int) {
+        self.linear = linear
+        self.exponential = exponential
+        self.start = start
     }
     
     // TODO: extract printing from this type?
     // Definitely refactor this this is ugly
-    static func printHeader() {
-        print(String(format: "%@\n| % 12@ | % 12@ | % 12@ |\n%@",
+    // Maybe define a print table Type
+    func header() -> String {
+        return String(format: "%@\n| % 12@ | % 12@ | % 12@ |\n%@",
                      String(repeating: "-", count: 12 * 3 + 4 + 6),
                      "x".padding(toLength: 12, withPad: " ", startingAt: 0),
                      "linear".padding(toLength: 12, withPad: " ", startingAt: 0),
                      "exponential".padding(toLength: 12, withPad: " ", startingAt: 0),
-                     String(repeating: "-", count: 12 * 3 + 4 + 6)))
+                     String(repeating: "-", count: 12 * 3 + 4 + 6)) + "\n"
     }
 }
 
 
-let linear = Growth(initialValue: 25_000, growth: 500, type: .linear)
-let exponential = Growth(initialValue: 20_000, growth: 1.05, type: .exponential)
-let startingYear = 1
+let linear = GrowthData(initialValue: 25_000, growth: 500, type: .linear)
+let exponential = GrowthData(initialValue: 20_000, growth: 1.05, type: .exponential)
+let growth = Growth(linear: linear, exponential: exponential, start: 1)
+print(growth.description)
 
-Growth.printResult(linear: linear, exponential: exponential, start: startingYear)
+
